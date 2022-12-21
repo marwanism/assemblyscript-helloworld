5475074b7dfeea172972cea53746fb5c365eeaa5
Marwan Sabry <shobo12maro@hotmail.com>
2022-12-21 10:23:52 +0000

    typo fix

--- assembly/index.ts
+++ assembly/index.ts
@@ -4,6 +4,6 @@ export function add(a: i32, b: i32): i32 {
   return a + b;
 }
 
-export function helloWorld(): String {
+export function helloWorld(): string {
   return "Hello from WebAssembly!";
 }
